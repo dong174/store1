4de5c7a795f6b3370cfc4f30b2d3da5510f6cf26
Dongwei <dongwei174110@163.com>
2021-08-26 09:52:10 +0800

    第二次作业

--- day2.py
+++ day2.py
@@ -1,2 +1,18 @@
-print(222222)
-print(222222)
+import random
+money = 100
+num = random.randint(0, 10)
+
+while money > 0:
+    a = input("请输入一个数字：")
+    a = int(a)
+    if a > num:
+        print("输入的数字过大")
+        money -= 10
+    elif a < num:
+        print("输入的数字过小")
+        money -= 10
+    else:
+        print("输入正确。")
+        break
+
+print("余额为：", money)
